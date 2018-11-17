b15cb6e7b526be23d069e87b94d0f3895a3ab1a1
PapoyanLyudvig <43209156+PapoyanLyudvig@users.noreply.github.com>
2018-11-17 15:27:29 +0400

    Add files via upload

---
+++ homework6/Bus.java
@@ -0,0 +1,21 @@
+public class Bus {
+	public int lenght;
+	public int height;
+	
+	Bus(int height, int lenght) {
+		this.lenght = lenght;
+		this.height = height;
+	}
+	public int getLenght() {
+		return lenght;
+	}
+	public double getHeight() {
+		return height;
+	}
+	public void drive() {
+		
+	}
+	public void stop() {
+
+	}
+}
---
+++ homework6/Car.java
@@ -0,0 +1,21 @@
+public class Car {
+	public int lenght;
+	public int height;
+	
+	Car(int height, int lenght) {
+		this.lenght = lenght;
+		this.height = height;
+	}	
+	public int getLenght() {
+		return lenght;
+	}
+	public double getHeight() {
+		return height;
+	}
+	public void drive() {
+		
+	}
+	public void stop() {
+
+	}
+}
